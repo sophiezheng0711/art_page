eaf56a69ffdbc75a3af4063ce56405b648eab330
sz374 <sz374@cornell.edu>
2019-06-27 19:33:31 -0400

    Updates

---
+++ precache-manifest.e8e86c2589818e492fca828b65cd5d61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ea8faae60a1322cc0b239f4eb1d298",
+    "url": "/art_page/index.html"
+  },
+  {
+    "revision": "6b79a9481e006291e846",
+    "url": "/art_page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a410a61e996963349ba7",
+    "url": "/art_page/static/js/2.fbb12d47.chunk.js"
+  },
+  {
+    "revision": "6b79a9481e006291e846",
+    "url": "/art_page/static/js/main.c495dfcf.chunk.js"
+  },
+  {
+    "revision": "c280221778597bf692b0",
+    "url": "/art_page/static/js/runtime~main.9462cf24.js"
+  }
+]);
